03802ff587377be09f98e7ca3c9f5e5b0a6f381a
NikitaGhimire <nikitaghimire1@gmail.com>
2025-02-02 19:09:49 -0800

    Frontend : Backend BaselUrl update to Heroku

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://msg-app-0e48ba52a8ee.herokuapp.com",
     headers: {
     'Content-Type': 'application/json',
 }, // Your backend API
